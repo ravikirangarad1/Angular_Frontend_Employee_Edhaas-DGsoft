f99e385406953d341905674e4928b526d9b2e754
ravikirangarad1 <ravikirangarad1@gmail.com>
2025-06-17 16:41:10 +0530

    Update ResourceNotFoundException.java

--- springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
@@ -3,6 +3,8 @@ package net.javaguides.springboot.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//this is global exception handle file
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
 
